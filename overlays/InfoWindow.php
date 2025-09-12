eb5c2361addce1053771c5f9958a54fdcb317ad6
webazex <webazex@gmail.com>
2025-09-12 10:43:49 +0300

    add $marker $content properties for InfoWindow.php

--- overlays/InfoWindow.php
+++ overlays/InfoWindow.php
@@ -1,67 +1,109 @@
 <?php
-
 /*
- *
  * @copyright Copyright (c) 2013-2019 2amigos
+ * @copyright Copyright (c) 2025 Latul Anton (webazex@gmail.com, https://latul.website)
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
- *
  */
 
 namespace dosamigos\google\maps\overlays;
 
+use dosamigos\google\maps\ObjectAbstract;
 use yii\base\InvalidConfigException;
-use yii\helpers\ArrayHelper;
 
 /**
- *
  * InfoWindow
  *
- * Google maps info window object.
+ * An InfoWindow displays content (usually text or images) in a popup window above the map, at a given location.
  *
  * @author Antonio Ramirez <hola@2amigos.us>
- *
+ * @author Latul Anton <webazex@gmail.com>
  * @link http://www.2amigos.us/
+ * @link https://latul.website/
  * @package dosamigos\google\maps
  */
-class InfoWindow extends InfoWindowOptions
+class InfoWindow extends ObjectAbstract
 {
     /**
-     * @inheritdoc
+     * @var string the content of the info window
+     */
+    public $content;
+
+    /**
+     * @var \dosamigos\google\maps\LatLng the position of the info window
+     */
+    public $position;
+
+    /**
+     * @var int the pixel offset for the tip of the info window
+     */
+    public $pixelOffset;
+
+    /**
+     * @var bool whether the info window is visible
+     */
+    public $visible = false;
+
+    /**
+     * @var int the z-index of the info window
+     */
+    public $zIndex;
+
+    /**
+     * @param array $config
+     */
+
+    /**
+     * @var string the marker (for JS)
+     */
+    public $marker;
+    public function __construct($config = [])
+    {
+        $this->options = array_merge([
+            'content' => null,
+            'position' => null,
+            'pixelOffset' => null,
+            'visible' => null,
+            'zIndex' => null,
+        ], $this->options);
+
+        parent::__construct($config);
+    }
+
+    /**
+     * @throws \yii\base\InvalidConfigException
+     * @return void
      */
     public function init()
     {
-        if ($this->content == null) {
-            throw new InvalidConfigException('"$content" cannot be null');
+        parent::init();
+        if ($this->content === null) {
+            throw new InvalidConfigException('"content" cannot be null');
         }
     }
 
     /**
-     * Returns the js to initialize the info window object
+     * Sets the content of the info window
+     * @param string $value
+     * @return void
+     */
+    public function setContent($value)
+    {
+        $this->content = $value;
+        $this->options['content'] = $value;
+    }
+
+    /**
+     * Returns the JavaScript code for the info window
+     * @param string $marker
      * @return string
      */
-    public function getJs()
+    public function getJs($marker = null)
     {
-        $js = [];
-
-        $js[] = "var {$this->getName()} = new google.maps.InfoWindow({$this->getEncodedOptions()});";
-
-        foreach ($this->events as $event) {
-            /** @var \dosamigos\google\maps\Event $event */
-            $js[] = $event->getJs($this->getName());
-        }
-
-        return implode("\n", $js);
-    }
-
-    /**
-     * Sets the options based on a InfoWindowOptions object
-     *
-     * @param InfoWindowOptions $infoWindowOptions
-     */
-    public function setOptions(InfoWindowOptions $infoWindowOptions)
-    {
-        $options = array_filter($infoWindowOptions->options);
-        $this->options = ArrayHelper::merge($this->options, $options);
+        $options = $this->options;
+        $options['map'] = $marker ?? $this->marker;
+        $js = "var {$this->name} = new google.maps.InfoWindow({$this->encode()});";
+        $js .= "{$this->name}.open({$options['map']}, {$marker});";
+        return $js;
     }
 }
